0c9187aad43b0f482a59e9e2126706e18592a864
luizink <luizupdate@gmail.com>
2017-11-08 11:41:34 -0500

    cambios slider

---
+++ module/footer.php
@@ -0,0 +1,53 @@
+   <footer id="footer">
+           
+        
+        <div id="footer-top">
+            <div class="container clearfix">
+                
+                <div class="one-fourth">
+                    
+                </div>
+               
+                <div class="one-fourth column-last">
+                    <div class="widget contact-info">
+                        <!--h3>Contact Info</h3-->
+                        <div>
+                            <img id="logo_blanco" src="images/logo/skl-blanco.svg">
+                        </div>
+
+                    </div>
+                </div>
+                <div class="one-fourth column-last">
+                    <div class="widget contact-info">
+                        <div>
+                            <div>
+                            <p class="address"><strong>Dirección</strong> Rosalía Lavalle 105 Ofic. 501, San Miguel, Lima, Perú</p>
+                            <p class="fax"><strong>Whatsapp:</strong> +51 946 574 032</p>
+                            <p class="email"><strong>Email:</strong> <a href="mailto:sklsoluciones@gmail.com">sklsoluciones@gmail.com</a></p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+        
+        <div id="footer-bottom">
+            <div class="container clearfix">
+                <div class="one-half">
+                    <p>Soluciones Gráficas SKL &copy; 2017</p>
+                </div>
+        
+                <div class="one-half column-last">
+                    <ul class="social-links">
+                        <!--li class="twitter"><a href="#" title="Twitter" target="_blank">Twitter</a></li-->
+                        <li class="facebook"><a href="#" title="Facebook" target="_blank">Facebook</a></li>
+                        <!--li class="google-plus"><a href="#" title="Google+" target="_blank">Google+</a></li-->
+                        <li class="linkedin"><a href="#" title="LinkedIn" target="_blank">LinkedIn</a></li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+       
+    </footer>
